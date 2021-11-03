9a4154c9d221b0ddccf7e1ba59b12ecf120e86a3
drive-knight <yavatar15@yandex.ru>
2021-11-04 01:44:50 +0500

    hello world

--- Parser.py
+++ Parser.py
@@ -1,11 +1,14 @@
 import re
+import os
 import requests
 import csv
+import time
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
-url = "https://www.detmir.ru/catalog/index/name/lego/"
+
+url = "https://www.detmir.ru/catalog/index/name/lego/page/1/"
 
 ua = UserAgent()
 
@@ -16,75 +19,78 @@ headers = {'User-Agent': ua.random,
            }
 
 cookies = dict(BCPermissionLevel='PERSONAL')
-response = requests.get(url, headers=headers, cookies=cookies)
+
+list_pages = []
+list_tags = []
+name = []
+price = []
+p_price = []
+link = []
+id = []
 
 
-with open('html.txt', 'w', encoding='utf-8') as f:
-    f.write(response.text)
-
-with open("html.txt", encoding='utf-8') as f:
-    soup = BeautifulSoup(f, 'html.parser')
-
-
-tags = soup.find(lambda tag: tag.name == 'div' and tag.get('class') == ['n_9'])
-
-link_list = []
-price_list = []
-p_price_list = []
-name_list = []
-id_list = []
-city_list = []
+def get_pages(lp: list):
+    page = 1
+    while page < 20:
+        new_page = "https://www.detmir.ru/catalog/index/name/lego/page/{}/".format(page)
+        page += 1
+        lp.append(new_page)
+    return lp
 
 
-el = 0
-try:
-    while el < 501:
-        for link in tags.contents[el]:
-            for a in link.find_all('a', href=True):
-                link_list.append(a['href'])
-                el += 1
-except IndexError:
-    pass
+def get_soup(lp: list):
+    for i in lp:
+        with open('html.txt', 'w', encoding='utf-8') as f:
+            response = requests.get(i, headers=headers, cookies=cookies)
+            f.write(response.text)
+            with open("html.txt", encoding='utf-8') as nf:
+                soup = BeautifulSoup(nf, 'html.parser')
+                tags = soup.find(lambda tag: tag.name == 'div' and tag.get('class') == ['n_6'])
 
-el = 0
-try:
-    while el < 501:
-        for p_price in tags.contents[el].find("p", class_="Oe"):
-            # p_price1 = re.sub(r'\D', '', p_price)
-            if p_price is not None:
-                p_price_list.append(p_price)
-                el += 1
-            else:
-                p_price_list.append("-")
-                el +=1
-except IndexError:
-    pass
+                #time.sleep(1)
 
-el = 0
-try:
-    while el < 501:
-        for price in tags.contents[el].find("p", class_="Of").find(class_='Og'):
-            # price1 = re.sub(r'\D', '', price)
-            price_list.append(price)
-            el += 1
-except IndexError:
-    pass
+                for names in tags.find_all('p', class_='NO'):
+                    name_re = re.sub(r'\d+$', '', *names)
+                    name.append(name_re)
+                    id_re = re.sub(r'\D', '', *names)
+                    if len(id_re) == 5:
+                        id.append(id_re)
+                    else:
+                        id.append(id_re[-5:])
 
-el = 0
-try:
-    while el < 501:
-        for name in tags.contents[el].p:
-            name_re = re.sub(r'[^\w\s]+|[\d]+', '', name)
-            id_re = re.sub(r'\D', '', name)
-            name_list.append(name_re)
-            id_list.append(id_re)
-            el += 1
-except IndexError:
-    pass
+                #time.sleep(1)
+
+                for p_prices in tags.find_all('p', class_='NZ'):
+                    p_price_re = re.sub(r'\D', '', *p_prices)
+                    p_price.append(p_price_re)
+
+                #time.sleep(1)
+
+                for prices in tags.find_all('span', class_='N_0'):
+                    price_re = re.sub(r'\D', '', *prices)
+                    price.append(price_re)
 
 
-with open('result.csv', 'w', newline='', encoding='utf-8') as csvf:
-    writer = csv.writer(csvf)   
-    writer.writerows([['id', 'title', 'price', 'promo_price', 'url']])
-    for line1, line2, line3, line4, line5 in zip(id_list, name_list, price_list, p_price_list, link_list):
-        writer.writerows([[line1, line2, line3, line4, line5]])
+                #time.sleep(1)
+
+                for links1 in tags:
+                    for links2 in links1.find_all('a', href=True):
+                        link.append(links2['href'])
+
+
+def cvs_writer(*args):
+    with open('result.csv', 'w', newline='', encoding='utf-8') as csvf:
+        writer = csv.writer(csvf)
+        writer.writerows([['id', 'title', 'price', 'promo_price', 'url']])
+        for line1, line2, line3, line4, line5 in zip(*args):
+            writer.writerows([[line1, line2, line3, line4, line5]])
+
+
+if __name__ == '__main__':
+    p = get_pages(list_pages)
+    get_soup(p)
+    cvs_writer(id, name, price, p_price, link)
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'html.txt')
+    os.remove(path)
+
+
